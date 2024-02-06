4e1cd97be1968a00854b3e313337f1c8dd1c7caa
Chartman97 <calebhartman97@gmail.com>
2024-02-05 19:13:02 -0700

    TemperatureConversionProject

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
-        public static void main(String[] args) {
-            // Mean Computer Original****************************
+    public static void main(String[] args) {
+        final double kelvin, celsius, fahrenheit;
+        Scanner in = new Scanner(System.in);
 
-            Scanner in = new Scanner (System.in);
-            System.out.println("Hi");
-            in.nextLine();
-            // Exchange of hello
-            String name;
-            System.out.println("...Ummm... Who are you?.. and don't even ask what my name is -_- ");
-            name = in.nextLine();
-            System.out.println( name + "...  That's your name? I am sorry about that. \n  Well, I guess im pretty bored. Wanna see a magic trick?");
-            // Asking of name & magic trick
-            String response;
-            response = in.nextLine();
-            System.out.println( "\"" + response + "\"" + ".. wow so excited. Okay, I could care less. Here is the magic trick.. ");
-            // First part of talk, intro and quick qip. Now time for "magic trick"
-            int inch;
-            double cm;
-            Scanner in2 = new Scanner(System.in) ;
+        // Get user to input data
+        // With a little friendly banter
+        System.out.println("\n\nHello! My name is \"The Temperature Conversion Machine\"!");
+        System.out.println("My only function is to convert Fahrenheit to Celsius & Kelvin");
+        System.out.print("Lets do some conversions! Input Value here -->");
+        fahrenheit = in.nextDouble();
 
-            System.out.print("How tall are you in inches... \nYes, do the math.. input it here ->") ;
-            inch = in.nextInt();
-            final double CMPERINCH = 2.45;
-            cm = inch * CMPERINCH;
+        // OutPut Screen Filler
+        System.out.print(" \n \n \n \n");
 
-            System.out.println("You are " + cm + " centimeters tall...") ;
-            System.out.println("Wow so cool, am i right!!");
+        // Convert Fahrenheit to Celsius & Kelvin
+        celsius = (double) ((( 5.0 / 9.0 ) * (fahrenheit - 32)));
+        kelvin = (double) (celsius + 273.15);
 
-            String Last;
-            Last = in.next();
-            Scanner in3 = new Scanner(System.in) ;
-            System.out.print("Okay... well GOOD BYE");
-            System.out.println(" ");
-            System.out.println(".......");
+        System.out.println("Boom");
+        System.out.println( fahrenheit + " Degrees Fahrenheit is:");
+        System.out.printf("Celsius = %.2f \nKelvin = %.2f\n", celsius, kelvin);
 
-            // Mean Computer Original****************************
-
-            // Scratch Code
-            // -----------
-            String name1;
-            name1 = "Caleb";
-            int feet1;
-            feet1 = inch / 12;
-            int inch1;
-            inch1 = in;
-            String height;
-            height = feet1 + "\'" + " " + inch1 + "\"" ;
-            System.out.println("so, i actually want to tell you something");
-            System.out.printf( "I really don't like %s \n" , name1 );
-            System.out.printf("He is only %s \n" , height);
-            System.out.print("Now if you lived in almost any other part of the world " + name1 + " would be " ) ;
-
-
-
-            // scratch Code
-
-
-
-
-        }
+        System.out.print("");
+    }
     }
